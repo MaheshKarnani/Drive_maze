9dec0d3e13952d1c528c0a322f3c5ef591f5a1b8
MaheshKarnani <36512140+MaheshKarnani@users.noreply.github.com>
2021-11-13 18:41:50 +0100

    Add files via upload

--- New_maze_v2.py
+++ New_maze_v2.py
@@ -1,6 +1,6 @@
-#new maze all on pi.
+#new maze all on pi. tested success.
 #2021Nov
-#11th
+#13th
 import serial
 # import keyboard
 import time
@@ -40,14 +40,14 @@ beaml=(b0,b1,b2,b3,b4,b5,b6,b7,b8,b9,b10,b11)
 #doors
 d0=10
 d1=11
-d2=0
-d3=1
-d4=2
-d5=3
-d6=4
-d7=5
-d8=6
-d9=7
+d2=6
+d3=7
+d4=4
+d5=5
+d6=2
+d7=3
+d8=0
+d9=1
 d10=8
 d11=9
 doorl=[d0,d1,d2,d3,d4,d5,d6,d7,d8,d9,d10,d11]
@@ -60,8 +60,9 @@ print(exit_doors)
 #set up gpio
 for x in range(0, 12):
     GPIO.setup(beaml[x], GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set an input pin and initial value to be pulled low (off)
-
-slowness=200 #milliseconds to wait between alternating doorset servo strokes
+for x in [3,5,7]:
+    GPIO.setup(beaml[x], GPIO.IN)
+slowness=4 #milliseconds to wait between alternating door servo strokes
 
 #open list
 target0 = 40
@@ -74,7 +75,7 @@ target6 = 40
 target7 = 165
 target8 = 20
 target9 = 57
-target10 = 175
+target10 = 178
 target11 = 170
 openl=[target0,target1,target2,target3,target4,target5,target6,target7,target8,target9,target10,target11]
 
@@ -94,19 +95,26 @@ target11 = 60
 closel=[target0,target1,target2,target3,target4,target5,target6,target7,target8,target9,target10,target11]
 
 #initialize current and target degrees for doors
-current=[90,90,90,90,90,90,90,90,90,90,90,90]
+current=[94,94,94,94,94,94,94,84,94,94,94,94]
 target=[90,90,90,90,90,90,90,90,90,90,90,90]
 
 d_turn=1
-d_stroke=1
-x=11
+d_stroke=[1,1,1,1,1,1,1,1,1,1,1,1]
+d=11
 
 millis = int(round(time.time() * 1000))
 door_timer=millis
-mode=-1
+mode=-6
 
 while True:
     #command loop changes targets
+    if mode==-6: #safe startup
+        slowness=500
+        d_state=np.subtract(target,current)
+        if max(abs(d_state))<3:
+            print("initialized")
+            slowness=4
+            mode=-1
     if mode==-1: #prep        
         target[doorl[2]] = closel[doorl[2]]
         target[doorl[3]] = openl[doorl[3]]
@@ -149,11 +157,11 @@ while True:
             mode=1
             print("m1")
             w=int(10)
+            millis = int(round(time.time() * 1000))
     if mode==1: #SEM trapped for id        
         target[doorl[0]] = closel[doorl[0]]
         target[doorl[1]] = closel[doorl[1]]
-        #id and wscan
-        millis = int(round(time.time() * 1000))
+        #id and wscan    
         generic_timer=int(round(time.time() * 1000))-millis
         if generic_timer>10000:
             w = int(20)
@@ -172,52 +180,52 @@ while True:
         target[doorl[1]] = openl[doorl[1]]
         mode=3
         print("m3")
-        print("in maze")
+        
         maze_entry_flag='False'
     if mode==3: #maze operational        
         if not maze_entry_flag and not GPIO.input(beaml[8]):# or ...: #enter any unit
             maze_entry_flag='True'
-
-#         if not GPIO.input(beaml[2]): #enter unit1
-#             print("unit1")
-#             target[doorl[2]] = closel[doorl[2]]
-#             target[doorl[3]] = openl[doorl[3]]
-#         if not GPIO.input(beaml[3]): #exit
-#             print("ex1")
-#             target[doorl[2]] = openl[doorl[2]]
-#             target[doorl[3]] = closel[doorl[3]]
-#         if not GPIO.input(beaml[4]): #enter unit2
-#             print("unit2")
-#             target[doorl[4]] = closel[doorl[4]]
-#             target[doorl[5]] = openl[doorl[5]]
-#         if not GPIO.input(beaml[5]): #exit
-#             print("ex2")
-#             target[doorl[4]] = openl[doorl[4]]
-#             target[doorl[5]] = closel[doorl[5]]
-#         if not GPIO.input(beaml[6]): #enter unit3
-#             print("unit3")
-#             target[doorl[6]] = closel[doorl[6]]
-#             target[doorl[7]] = openl[doorl[7]]
-#         if not GPIO.input(beaml[7]): #exit
-#             print("ex3")
-#             target[doorl[6]] = openl[doorl[6]]
-#             target[doorl[7]] = closel[doorl[7]]   
-        if not GPIO.input(beaml[8]): #enter unit4
+            print("in maze")
+        if not GPIO.input(beaml[2]): #enter unit1
+            print("unit1")
+            target[doorl[3]] = closel[doorl[3]]
+            target[doorl[2]] = openl[doorl[2]]
+        if not GPIO.input(beaml[3]): #exit
+            print("ex1")
+            target[doorl[3]] = openl[doorl[3]]
+            target[doorl[2]] = closel[doorl[2]]
+        if not GPIO.input(beaml[4]): #enter unit2
+            print("unit2")
+            target[doorl[5]] = closel[doorl[5]]
+            target[doorl[4]] = openl[doorl[4]]
+        if not GPIO.input(beaml[5]): #exit
+            print("ex2")
+            target[doorl[5]] = openl[doorl[5]]
+            target[doorl[4]] = closel[doorl[4]]
+        if not GPIO.input(beaml[6]): #enter unit3
+            print("unit3")
+            target[doorl[7]] = closel[doorl[7]]
+            target[doorl[6]] = openl[doorl[6]]
+        if not GPIO.input(beaml[7]): #exit
+            print("ex3")
+            target[doorl[7]] = openl[doorl[7]]
+            target[doorl[6]] = closel[doorl[6]]   
+        if not GPIO.input(beaml[9]): #enter unit4
             print("unit4")
-            target[doorl[8]] = closel[doorl[8]]
-            target[doorl[9]] = openl[doorl[9]]
-        if not GPIO.input(beaml[9]): #exit
-            print("ex4")
+            target[doorl[9]] = closel[doorl[9]]
             target[doorl[8]] = openl[doorl[8]]
-            target[doorl[9]] = closel[doorl[9]]   
+        if not GPIO.input(beaml[8]): #exit
+            print("ex4")
+            target[doorl[9]] = openl[doorl[9]]
+            target[doorl[8]] = closel[doorl[8]]   
         if not GPIO.input(beaml[10]): #enter unit5
             print("unit5")
-            target[doorl[10]] = closel[doorl[10]]
-            target[doorl[11]] = openl[doorl[11]]
+            target[doorl[11]] = closel[doorl[11]]
+            target[doorl[10]] = openl[doorl[10]]
         if not GPIO.input(beaml[11]): #exit
             print("ex5")
-            target[doorl[10]] = openl[doorl[10]]
-            target[doorl[11]] = closel[doorl[11]]   
+            target[doorl[11]] = openl[doorl[11]]
+            target[doorl[10]] = closel[doorl[10]]   
         
         if not GPIO.input(beaml[0]) and maze_entry_flag: #leave maze
             target[doorl[0]] = openl[doorl[0]]
@@ -241,28 +249,19 @@ while True:
     #set up a door loop. if not at target, move to target. two at a time.
     d_state=np.subtract(target,current)
     if max(abs(d_state))>2:                 #if doors are not at target
-        x=x+1
-        if x==12:   #cycle through doors
-            x=0
-        if abs(d_state[x])>2: #find door not at target
-            millis = int(round(time.time() * 1000))
-            if millis-door_timer>slowness:      #if movement is slow enough        
-#             if d_turn==1:                   #if it's this doorset's turn
-#                 doorset=entry_doors
-#                 d_turn=2
-#             else:                           #or the other doorset
-#                 doorset=exit_doors
-#                 d_turn=1
-#             for x in doorset:               #find door out of target
-#                 d_state=np.subtract(target,current)
-#                 if abs(d_state[x])>2:       #
-                if d_stroke==1:         #if it's this stroke direction's turn
-                    current[x] = current[x]+3*(target[x]-current[x])/abs(target[x]-current[x])
-                    d_stroke=2
+        millis = int(round(time.time() * 1000))
+        if millis-door_timer>slowness:      #if movement is slow enough  
+            d=d+1      #cycle through doors
+            if d==12:  #
+                d=0    #
+            if abs(d_state[d])>2: #if this door not at target
+                if d_stroke[d]==1:         #if it's this stroke direction's turn
+                    current[d] = current[d]+4*(target[d]-current[d])/abs(target[d]-current[d])
+                    d_stroke[d]=2
                 else:                   #or the other stroke direction
-                    current[x] = current[x]-2*(target[x]-current[x])/abs(target[x]-current[x])
-                    d_stroke=1
-                kit.servo[x].angle = current[x]             #movement                    
+                    current[d] = current[d]-2*(target[d]-current[d])/abs(target[d]-current[d])
+                    d_stroke[d]=1
+                kit.servo[d].angle = current[d]             #movement                    
                 door_timer=millis
 
 
