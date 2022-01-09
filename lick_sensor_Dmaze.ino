b362c09bdd94fb09fff0d0171aec64bad5c45358
MaheshKarnani <36512140+MaheshKarnani@users.noreply.github.com>
2022-01-09 10:53:36 +0100

    Add files via upload

---
+++ lick_sensor_Dmaze.ino
@@ -0,0 +1,50 @@
+#include <CapacitiveSensor.h>
+CapacitiveSensor   cs_6_7 = CapacitiveSensor(6,7);       // 10M resistor between pins 6 & 7, pin 7 is sensor pin
+//Capacitive sensor
+int cs_thresh;//look at serial monitor during testing to find a value
+int var;
+int cs1;
+//outputs to Pi
+const int ard_pi_lick = 2;//reports capacitive sensor to Pi
+const int led = 3;//diagnostic led
+
+void setup() 
+{
+    cs_6_7.set_CS_AutocaL_Millis(0xFFFFFFFF);     // turn off autocalibrate on channel 1 - just as an example
+    Serial.begin(9600);//setup serial
+    pinMode(ard_pi_lick, OUTPUT);
+    pinMode(led, OUTPUT);
+    //collect lick sensor bsl
+    delay(500);
+    var = 0;
+    cs_thresh=0;
+    while (var < 5) 
+    {
+      cs_thresh =  cs_thresh+cs_30_31.capacitiveSensor(30);
+      var++;
+    }
+    cs_thresh=(cs_thresh/5)+300;
+    delay(100);
+    //Serial.print("cs_threshold   ");  //show value for trouble shooting if serial monitor is on
+    //Serial.println(cs_thresh); 
+}
+
+void loop() 
+{
+    long cs1 =  cs_6_7.capacitiveSensor(30);
+    Serial.print("\t");                    // tab character for debug windown spacing
+    Serial.print(cs1); 
+    
+    if (cs1<cs_thresh) //capacitive sensor
+    {
+        digitalWrite(ard_pi_lick, LOW);
+        digitalWrite(led, HIGH);
+    }  
+    else
+    {
+        digitalWrite(ard_pi_lick, HIGH);
+        digitalWrite(led, LOW);
+    } 
+
+    
+}
