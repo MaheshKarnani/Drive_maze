979bc63b9bc4f57c26cfec0cb947d42772d6aec4
MaheshKarnani <36512140+MaheshKarnani@users.noreply.github.com>
2022-02-06 15:38:21 +0100

    Update lick_sensor_Dmaze.ino
    works on arduino uno usb to pi and drop stop on water line

--- lick_sensor_Dmaze.ino
+++ lick_sensor_Dmaze.ino
@@ -11,39 +11,40 @@ const int led = 3;//diagnostic led
 void setup() 
 {
     cs_6_7.set_CS_AutocaL_Millis(0xFFFFFFFF);     // turn off autocalibrate on channel 1 - just as an example
-    Serial.begin(9600);//setup serial
+//    Serial.begin(9600);//setup serial
     pinMode(ard_pi_lick, OUTPUT);
     pinMode(led, OUTPUT);
     //collect lick sensor bsl
-    delay(500);
+    delay(1000);
     var = 0;
     cs_thresh=0;
     while (var < 5) 
     {
-      cs_thresh =  cs_thresh+cs_30_31.capacitiveSensor(30);
+      cs_thresh =  cs_thresh+cs_6_7.capacitiveSensor(10); //resolution 10
       var++;
     }
-    cs_thresh=(cs_thresh/5)+300;
-    delay(100);
-    //Serial.print("cs_threshold   ");  //show value for trouble shooting if serial monitor is on
-    //Serial.println(cs_thresh); 
+    cs_thresh=(cs_thresh/5)+30;
+    delay(1000);
+//    Serial.print("cs_threshold   ");  //show value for trouble shooting if serial monitor is on
+//    Serial.println(cs_thresh); 
 }
 
 void loop() 
 {
-    long cs1 =  cs_6_7.capacitiveSensor(30);
-    Serial.print("\t");                    // tab character for debug windown spacing
-    Serial.print(cs1); 
+    long cs1 = cs_6_7.capacitiveSensor(10); //resolution 10
+    var = 0;
+//    Serial.print("\t");                    // tab character for debug windown spacing
+//    Serial.println(cs1); 
     
     if (cs1<cs_thresh) //capacitive sensor
     {
         digitalWrite(ard_pi_lick, LOW);
-        digitalWrite(led, HIGH);
+        digitalWrite(led, LOW);
     }  
     else
     {
         digitalWrite(ard_pi_lick, HIGH);
-        digitalWrite(led, LOW);
+        digitalWrite(led, HIGH);
     } 
 
     
